6cd332a26073086a290ac9db93685dff105f6e82
Sebastian Carroll <seabass25@hotmail.com>
2021-06-03 18:31:05 +0100

    Added logic to change schedule

--- index.mjs
+++ index.mjs
@@ -9,20 +9,34 @@ const options = {
 
 var stream = fs.createWriteStream("append.csv", { flags: 'a' });
 
-const job = schedule.scheduleJob('*/30 * * * *', async () => {
+const every_30_mins = '*/30 * * * *';
+const every_5_mins = '*/5 * * * *';
+let in_fast_mode = false;
+
+const job = schedule.scheduleJob(every_30_mins, async () => {
 //{const job = schedule.scheduleJob('* * * * *', async () => {
-  let mpbs = 0;
+  let mbps = 0;
   let lat = null;
   try {
     console.log(`starting at ${new Date().toISOString()}`);
     const { download, ping : { latency} } = await speedTest(options);
     lat = latency;
-    mpbs = (download?.bandwidth / 1024 / 1024) * 8;
-    console.log(`Speed was ${mpbs}, latency was ${latency}`);
+    mbps = (download?.bandwidth / 1024 / 1024) * 8;
+    console.log(`Speed was ${mbps}, latency was ${latency}`);
   } catch (err) {
     console.log(err.message);
   }
-  stream.write(`${new Date().toISOString()},${mpbs},${lat}\n`);
+
+  stream.write(`${new Date().toISOString()},${mbps},${lat}\n`);
+
+  if(mbps < 175 && !in_fast_mode) {
+    console.log('Looks slow - entering fast mode');
+    in_fast_mode = true;
+    job.reschedule(every_5_mins);
+  } else if(mbps > 175 && in_fast_mode) {
+    console.log('Looks to have recovered - back to slow mode');
+    job.reschedule(every_30_mins);
+  }
 })
 
 process.on('SIGINT', () => {
